6fea1bac8e1d055f5ed097bbeacbc6b6224f0dab
SoraKotori <38377583+SoraKotori@users.noreply.github.com>
2025-04-27 15:57:21 +0000

    Refactor waitable_map: improve coroutine handling and fix comments for clarity

--- waitable_map.hpp
+++ waitable_map.hpp
@@ -11,7 +11,7 @@ public:
     using map_key_type = typename map_container_type::key_type;
     using map_iterator = typename map_container_type::iterator;
 
-    // coroutine chain: coroutine3 -> coroutine2 -> coroutine1 -> noop
+    // coroutine chain: coroutine3 -> coroutine2 -> coroutine1 -> nullptr
     using coroutine_container_type = MapContainer<Key, std::coroutine_handle<>, ContainerArgs...>;
     using coroutine_key_type = typename coroutine_container_type::key_type;
 
@@ -21,7 +21,7 @@ public:
         // 因此無法保存在 map_awaiter 中，作為 await_resume 時所使用，解決方法:
         // 1. 保存 container 和 key，每次重新查詢
         // 2. 改用 std::map 基於 node-based 的方法
-        // 3. 使用 stable_vector 或 heap，確保 iterators 和 references 不會失效
+        // 3. 使用 boost::stable_vector 或 std::unique_ptr，確保 iterators 和 references 不會失效
         struct await_key
         {
             const map_container_type& map;
@@ -37,15 +37,18 @@ public:
         }
 
         template<typename Promise>
-        auto await_suspend(std::coroutine_handle<Promise> current_handle)
+        auto await_suspend(std::coroutine_handle<Promise> current_handle) -> std::coroutine_handle<>
         {
             auto [_, coroutines, key] = std::get<await_key>(variant);
 
-            auto&  resume_handle = current_handle.promise().resume_handle();
+            auto&  resume_handle = current_handle.promise().resume_handle;
             auto& waiting_handle = coroutines.try_emplace(key, std::noop_coroutine()).first;
 
             // resume_handle <- waiting_handle <- current_handle
-            return std::exchange(resume_handle, std::exchange(waiting_handle, current_handle));
+            if (auto handle = std::exchange(resume_handle, std::exchange(waiting_handle, current_handle)))
+                return handle;
+            else
+                return std::noop_coroutine();
         }
 
         auto await_resume() const
@@ -63,8 +66,8 @@ public:
         if (!pair.second) // pair.bool
             return pair;
 
-        if (auto node = coroutines_.extract(pair.first->first)) // pair.iterator->key_type
-            node.mapped.resume(); // handle.resume
+        if (auto node = coroutines_.extract(std::forward<K>(key)))
+            node.mapped.resume(); // handle.resume()
 
         return pair;
     }
@@ -82,22 +85,43 @@ private:
     coroutine_container_type coroutines_;
 };
 
-struct Task
+struct promise
 {
-    class promise_type
+    using coroutine_handle_type = std::coroutine_handle<promise>;
+
+    coroutine_handle_type resume_handle;
+
+    auto get_return_object()
     {
-        std::coroutine_handle<promise_type> resume_handle_;
-
-    public:
-        Task get_return_object() { return {}; };
-        auto initial_suspend()   { return std::suspend_never{}; }
-        auto final_suspend()     { return std::suspend_never{}; }
-
-        auto& resume_handle()    { return resume_handle_; }
+        return coroutine_handle_type::from_promise(*this);
     };
+
+    auto initial_suspend()
+    {
+        return std::suspend_never{};
+    }
+
+    // 若非回傳 std::suspend_never，則需要手動呼叫 .destroy()
+    auto final_suspend() -> std::coroutine_handle<>
+    {
+        if (resume_handle)
+            return resume_handle;
+        else
+            return std::noop_coroutine();
+    }
+
+    void return_void() {}
 };
 
-Task coroutine(waitable_map<std::unordered_map, __u64, path>& map)
+template<typename Promise>
+struct Task : std::coroutine_handle<>
 {
-    return {};
+    using promise_type = Promise;
+};
+
+Task<promise> coroutine()
+{
+    // waitable_map<std::unordered_map, __u64, path> map;
+
+    co_return;
 }
