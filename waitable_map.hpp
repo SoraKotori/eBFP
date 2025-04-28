16b944032a4d2ad7373c1d02fbad425e48bcf90b
sorakotori <sora.kotori.phone@gmai.com>
2025-04-28 06:06:33 +0000

    Refactor waitable_map: restructure class definition and improve async_find method for better coroutine handling

--- waitable_map.hpp
+++ waitable_map.hpp
@@ -1,90 +1,5 @@
 #include <unordered_map>
 
-template<template<typename...> typename MapContainer,
-         typename Key,
-         typename T,
-         typename... ContainerArgs>
-class waitable_map
-{
-public:
-    using map_container_type = MapContainer<Key, T, ContainerArgs...>;
-    using map_key_type = typename map_container_type::key_type;
-    using map_iterator = typename map_container_type::iterator;
-
-    // coroutine chain: coroutine3 -> coroutine2 -> coroutine1 -> nullptr
-    using coroutine_container_type = MapContainer<Key, std::coroutine_handle<>, ContainerArgs...>;
-    using coroutine_key_type = typename coroutine_container_type::key_type;
-
-    struct map_awaiter
-    {
-        // coroutine container 插入元素後，原本的 iterators 和 references 可能會失效，
-        // 因此無法保存在 map_awaiter 中，作為 await_resume 時所使用，解決方法:
-        // 1. 保存 container 和 key，每次重新查詢
-        // 2. 改用 std::map 基於 node-based 的方法
-        // 3. 使用 boost::stable_vector 或 std::unique_ptr，確保 iterators 和 references 不會失效
-        struct await_key
-        {
-            const map_container_type& map;
-            coroutine_container_type& coroutines;
-            const coroutine_key_type& key;
-        };
-
-        std::variant<map_iterator, await_key> variant;
-
-        auto await_ready() const
-        {
-            return std::holds_alternative<map_iterator>(variant);
-        }
-
-        template<typename Promise>
-        auto await_suspend(std::coroutine_handle<Promise> current_handle) -> std::coroutine_handle<>
-        {
-            auto [_, coroutines, key] = std::get<await_key>(variant);
-
-            auto&  resume_handle = current_handle.promise().resume_handle;
-            auto& waiting_handle = coroutines.try_emplace(key, std::noop_coroutine()).first;
-
-            // resume_handle <- waiting_handle <- current_handle
-            if (auto handle = std::exchange(resume_handle, std::exchange(waiting_handle, current_handle)))
-                return handle;
-            else
-                return std::noop_coroutine();
-        }
-
-        auto await_resume() const
-        {
-            return std::holds_alternative<map_iterator>(variant) ?
-                std::get<map_iterator>(variant) :
-                std::get<await_key>(variant).map.find(std::get<await_key>(variant).key);
-        }
-    };
-
-    template<typename K, typename... Args>
-    auto try_emplace(K&& key, Args&&... args)
-    {
-        auto pair = map_.try_emplace(std::forward<K>(key), std::forward<Args>(args)...);
-        if (!pair.second) // pair.bool
-            return pair;
-
-        if (auto node = coroutines_.extract(std::forward<K>(key)))
-            node.mapped.resume(); // handle.resume()
-
-        return pair;
-    }
-
-    template<typename K>
-    auto async_find(const K& key)
-    {
-        auto   map_it = map_.find(key);
-        return map_it == std::end(map_) ? map_awaiter{map_, coroutines_, key}
-                                        : map_awaiter{map_it};
-    }
-
-private:
-    map_container_type map_;
-    coroutine_container_type coroutines_;
-};
-
 template<typename Promise>
 struct Task : std::coroutine_handle<>
 {
@@ -149,9 +64,101 @@ struct promise
     auto unhandled_exception() {}
 };
 
-Task<promise> coroutine()
+template<template<typename...> typename MapContainer,
+         typename ContainerKey,
+         typename T,
+         typename... ContainerArgs>
+class waitable_map
 {
-    waitable_map<std::unordered_map, __u64, path> map;
+public:
+    using map_container_type = MapContainer<ContainerKey, T, ContainerArgs...>;
+    using map_key_type = typename map_container_type::key_type;
+    using map_iterator = typename map_container_type::iterator;
+
+    // coroutine chain: coroutine3 -> coroutine2 -> coroutine1 -> nullptr
+    using coroutine_container_type = MapContainer<ContainerKey, std::coroutine_handle<promise>, ContainerArgs...>;
+    using coroutine_key_type = typename coroutine_container_type::key_type;
+
+    // coroutine container 插入元素後，原本的 iterators 和 references 可能會失效，
+    // 因此無法保存在 map_awaiter 中，作為 await_resume 時所使用，解決方法:
+    // 1. 保存 container 和 key，每次重新查詢
+    // 2. 改用 std::map 基於 node-based 的方法
+    // 3. 使用 boost::stable_vector 或 std::unique_ptr，確保 iterators 和 references 不會失效
+    template<typename Key>
+    struct await_key
+    {
+        const map_container_type& map;
+        coroutine_container_type& coroutines;
+        const Key& key;
+    };
+
+    template<typename Key>
+    struct map_awaiter : std::variant<map_iterator, await_key<Key>>
+    {
+        // 使用 base::constructor 來初始化 map_awaiter
+        using std::variant<map_iterator, await_key<Key>>::variant;
+
+        auto await_ready() const
+        {
+            return std::holds_alternative<map_iterator>(*this);
+        }
+
+        template<typename Promise>
+        auto await_suspend(std::coroutine_handle<Promise> current_handle) -> std::coroutine_handle<>
+        {
+            auto [_, coroutines, key] = std::get<await_key<Key>>(*this);
+
+            auto&  resume_handle = current_handle.promise().resume_handle;
+            auto& waiting_handle = coroutines[key];
+
+            // resume_handle <- waiting_handle <- current_handle
+            if (auto handle = std::exchange(resume_handle, std::exchange(waiting_handle, current_handle)))
+                return handle;
+            else
+                return std::noop_coroutine();
+        }
+
+        auto await_resume() const
+        {
+            return std::holds_alternative<map_iterator>(*this) ?
+                std::get<map_iterator>(*this) :
+                std::get<await_key<Key>>(*this).map.find(std::get<await_key<Key>>(*this).key);
+        }
+    };
+
+    template<typename K, typename... Args>
+    auto try_emplace(K&& key, Args&&... args)
+    {
+        auto pair = map_.try_emplace(std::forward<K>(key), std::forward<Args>(args)...);
+        if (!pair.second) // pair.bool
+            return pair;
+
+        if (auto node = coroutines_.extract(std::forward<K>(key)))
+            node.mapped.resume(); // handle.resume()
+
+        return pair;
+    }
+
+    template<typename Key>
+    auto async_find(const Key& key)
+    {
+        auto map_it = map_.find(key);
+        if  (map_it == std::end(map_))
+            return map_awaiter<Key>{std::in_place_type<await_key<Key>>, map_, coroutines_, key};
+        else
+            return map_awaiter<Key>{map_it};
+    }
+
+private:
+    map_container_type map_;
+    coroutine_container_type coroutines_;
+};
+
+Task<promise> coroutine(waitable_map<std::unordered_map, __u64, std::string>& map)
+{
+    auto iterator = co_await map.async_find(10);
+
+    std::println("find string: {}", iterator->second);
 
     co_return;
 }
